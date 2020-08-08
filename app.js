718a4f8d55a35e487534694ce7ab2419e80dcd34
Greg Hawthorne <ghawthorne@me.com>
2020-08-08 12:06:44 -0700

    delete on UI

--- app.js
+++ app.js
@@ -51,6 +51,22 @@ var Expense = function(id, description, value) {
             //Return the new element
             return newItem;
         },
+        deleteItem: function(type, id) {
+            var ids, index;
+            // id = 6
+            // data.allItems[type][id];
+            //ids = [1 2 4 6 8]
+            //index = 3
+           var ids =  data.allItems[type].map(function(current) {
+                return current.id;
+            });
+           index = ids.indexOf(id);
+
+           if (index !== -1) {
+            data.allItems[type].splice(index, 1);
+           }
+
+        },
         calculateBudget: function() {
             //calculate total income and expenses
         calculateTotal('exp');
@@ -124,6 +140,11 @@ var  UIController = (function () {
           //insert html into the DOM
           document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
       },
+      deleteListItem: function (selectorID) {
+          var el = document.getElementById(selectorID);
+          el.parentNode.removeChild(el);
+      },
+
       clearFields: function () {
           var fields, fieldsArr;
 
@@ -203,11 +224,14 @@ var controller = (function (budgetCtrl, UICtrl) {
         if (itemID) {
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             //1. delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
             //2. Delete the item from the UI
+            UICtrl.deleteListItem(itemID);
             //3. Update and show the new budget
+            updateBudget();
         }
     }
     return {
